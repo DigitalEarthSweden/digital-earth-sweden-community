2bca9d30c732b90a5cfb66c5ec5c8497d013330c
Daniel Flemström <daniel.flemstrom@ri.se>
2024-09-03 11:03:45 +0200

    Updated Docker stuff to make it easier to start a container

--- tutorials/start_notebook.sh
+++ tutorials/start_notebook.sh
@@ -6,18 +6,16 @@ NOTEBOOK_PORT=8888
 HOST_DIR=$(pwd)  # The current directory
 CONTAINER_DIR="/proj"  # Directory inside the container
 
-# Run the Docker container with the notebook server
+# Run the Docker container
 docker run --rm -it \
     -p $NOTEBOOK_PORT:8888 \
     --name eo-training-notebook \
     --mount type=bind,source=$HOST_DIR,target=$CONTAINER_DIR \
-    $IMAGE_NAME \
-    bash -c "jupyter lab --port=8888 --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --notebook-dir=$CONTAINER_DIR"
+    $IMAGE_NAME
 
 # Explanation:
 # - `--rm`: Automatically remove the container when it exits.
 # - `-it`: Interactive terminal.
 # - `-p 8888:8888`: Map port 8888 on the host to port 8888 in the container.
 # - `--mount`: Mount the current directory into the container at /proj.
-# - `$IMAGE_NAME`: The Docker image to run.
-
+# - `$IMAGE_NAME`: The Docker image to run, which now automatically starts Jupyter Lab.
